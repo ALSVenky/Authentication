b0cb52593be87103671516e841a1398ca7bb3c30
ALSVenky <atukurivenkatesh2@gmail.com>
2022-07-07 17:00:41 +0530

    level-3 hashing with md5

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
-
+const md5=require("md5");
 const app=express();
 app.use(express.static("public"));
 app.set("view engine","ejs");
@@ -18,7 +17,6 @@ const usersSchema=new mongoose.Schema({
     password:String
 });
 
-usersSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]})
 const User=mongoose.model("User",usersSchema);
 
 app.get("/",function(req,res){
@@ -36,7 +34,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const user1=new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     })
     user1.save(function(err){
         if(err){
@@ -49,7 +47,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username=req.body.username;
-    const password=req.body.password;
+    const password=md5(req.body.password);
 
     User.findOne({email:username},function(err,foundUser){
         if(err){
